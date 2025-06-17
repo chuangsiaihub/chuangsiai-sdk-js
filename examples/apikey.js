0f699b6937ce7db287fe2fd84042c951c39c8ff5
biubiuuuu <1803854237@qq.com>
2025-06-17 21:05:31 +0800

    update

--- examples/apikey.js
+++ examples/apikey.js
@@ -1,10 +1,6 @@
-import { ChuangsiaiClient } from '../dist/index.js';
 
-const client = new ChuangsiaiClient({ apiKey: '<apiKey>' });
+import { ChuangsiaiClient } from 'chuangsiai-sdk-test';
+// const { ChuangsiaiClient } = require('chuangsiai-sdk-test');
 
-client
-    .inputGuardrail({ strategyKey: '<strategyKey>', content: '检测文本内容' })
-    .then((res) => {
-        console.log(res)
-    })
-    .catch(console.error);
+const client = new ChuangsiaiClient({ accessKey: 'accessKey', secretKey: 'secretKey' });
+client.inputGuardrail({ strategyKey: 'strategyKey', content: '检测文本内容' }).then(console.log).catch(console.error);
