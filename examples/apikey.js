1ea2a745fb7a6b1482798ea488ad58ad7bc9469c
biubiuuuu <1803854237@qq.com>
2025-06-20 16:40:32 +0800

    修正导入路径，使用正确的 'chuangsiai-sdk' 模块

--- examples/apikey.js
+++ examples/apikey.js
@@ -1,6 +1,6 @@
 
-import { ChuangsiaiClient } from 'chuangsiai-sdk-test';
-// const { ChuangsiaiClient } = require('chuangsiai-sdk-test');
+import { ChuangsiaiClient } from 'chuangsiai-sdk';
+// const { ChuangsiaiClient } = require('chuangsiai-sdk');
 
 const client = new ChuangsiaiClient({ accessKey: 'accessKey', secretKey: 'secretKey' });
 client.inputGuardrail({ strategyKey: 'strategyKey', content: '检测文本内容' }).then(console.log).catch(console.error);
